c1058d596e409f2d5790bef8b75f21d23e18d174
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-02 13:04:48 -0400

    Added a customize Appearance class

---
+++ Events/Appearance.h
@@ -0,0 +1,17 @@
+//
+//  Appearance.h
+//  Events
+//
+//  Created by Rahul Nadella on 5/2/15.
+//  Copyright (c) 2015 Rahul Nadella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Appearance : NSObject
+
++ (void)customizeNavigationAppearance;
+
++ (void)customizeNavigationBar;
+
+@end
---
+++ Events/Appearance.m
@@ -0,0 +1,46 @@
+//
+//  Appearance.m
+//  Events
+//
+//  Created by Rahul Nadella on 5/2/15.
+//  Copyright (c) 2015 Rahul Nadella. All rights reserved.
+//
+
+#import "Appearance.h"
+
+@implementation Appearance
+
++ (void)customizeNavigationAppearance
+{
+    /* Create resizable images */
+    UIImage *gradientImage44 = [[UIImage imageNamed:@"surf_gradient_textured_44"]
+                                resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
+    
+    // Set the background image for *all* UINavigationBars
+    [[UINavigationBar appearance] setBackgroundImage:gradientImage44
+                                       forBarMetrics:UIBarMetricsDefault];
+    
+    /* Create the desired color */
+    UIColor *color = [UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1.0];
+    /* Create the Shadow Color and Offset */
+    NSShadow *shadow = [NSShadow new];
+    [shadow setShadowColor: [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8]];
+    [shadow setShadowOffset: CGSizeMake(0.0f, -1.0f)];
+    /* Create the desired FONT for the Navigation Bar */
+    UIFont *font = [UIFont fontWithName:@"Cochin-BoldItalic" size:20.0];
+    /* Map of attributes to define look of Navigation Bar */
+    NSMutableDictionary *attributes = [NSMutableDictionary dictionaryWithCapacity:1];
+    [attributes setObject:color forKey:NSForegroundColorAttributeName];
+    [attributes setObject:shadow forKey:NSShadowAttributeName];
+    [attributes setObject:font forKey:NSFontAttributeName];
+    
+    /* Customize the title text for *all* UINavigationBars */
+    [[UINavigationBar appearance] setTitleTextAttributes:attributes];
+}
+
++ (void)customizeNavigationBar
+{
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+}
+
+@end
